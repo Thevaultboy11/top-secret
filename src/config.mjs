a41717696e417656cde7964aa40ab9f95ad887ef
TheVaultboy11 <thevaultboy11@gmail.com>
2023-09-02 01:37:07 +0200

    adding the seo changes

--- src/config.mjs
+++ src/config.mjs
@@ -1,54 +1,54 @@
-import defaultImage from './assets/images/hero.jpg';
+import defaultImage from "/images/hero1.png";
 
 const CONFIG = {
-  origin: 'https://www.starshipshield.com',
-  basePathname: '/',
+  origin: "https://www.starshipshield.com",
+  basePathname: "/",
   trailingSlash: false,
 
-  title: 'Starship Shield',
+  title: "Starship Shield",
   description:
-    '🚀 Empowering cyber security solutions by young minds. Claim your free security assessment. Limited availability.',
+    "🚀 Empowering cyber security solutions by young minds. Claim your free security assessment. Limited availability.",
   defaultImage: defaultImage,
 
-  defaultTheme: 'light:only', // Values: "system" | "light" | "dark" | "light:only" | ""
+  defaultTheme: "light:only", // Values: "system" | "light" | "dark" | "light:only" | ""
 
-  language: 'en',
-  textDirection: 'ltr',
+  language: "en",
+  textDirection: "ltr",
 
-  dateFormatter: new Intl.DateTimeFormat('en', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    timeZone: 'UTC',
+  dateFormatter: new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    timeZone: "UTC",
   }),
 
   googleAnalyticsId: false, // or "G-XXXXXXXXXX",
-  googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
+  googleSiteVerificationId: "orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M",
 
   blog: {
     disabled: false,
     postsPerPage: 4,
 
     post: {
-      permalink: '/%slug%', // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
+      permalink: "/%slug%", // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
       noindex: false,
       disabled: false,
     },
 
     list: {
-      pathname: 'blog', // Blog main path, you can change this to "articles" (/articles)
+      pathname: "blog", // Blog main path, you can change this to "articles" (/articles)
       noindex: false,
       disabled: false,
     },
 
     category: {
-      pathname: 'category', // Category main path /category/some-category
+      pathname: "category", // Category main path /category/some-category
       noindex: true,
       disabled: false,
     },
 
     tag: {
-      pathname: 'tag', // Tag main path /tag/some-tag
+      pathname: "tag", // Tag main path /tag/some-tag
       noindex: true,
       disabled: false,
     },
