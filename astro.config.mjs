c3f6e658879dd0c8036e60160e40f24f9b73eec5
voodoohacker <thevaultboy11@gmail.com>
2023-11-06 10:33:33 +0100

    adding the product page and web page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,34 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import image from '@astrojs/image';
-import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
-import compress from 'astro-compress';
-import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
-import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import compress from "astro-compress";
+import { readingTimeRemarkPlugin } from "./src/utils/frontmatter.mjs";
+import react from "@astrojs/react";
 
-import { SITE } from './src/config.mjs';
+import { SITE } from "./src/config.mjs";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const whenExternalScripts = (items = []) =>
-  SITE.googleAnalyticsId ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
+  SITE.googleAnalyticsId
+    ? Array.isArray(items)
+      ? items.map((item) => item())
+      : [items()]
+    : [];
 
 export default defineConfig({
   site: SITE.origin,
   base: SITE.basePathname,
-  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
+  trailingSlash: SITE.trailingSlash ? "always" : "never",
 
-  output: 'static',
+  output: "static",
 
   markdown: {
     remarkPlugins: [readingTimeRemarkPlugin],
@@ -39,13 +43,13 @@ export default defineConfig({
     }),
     sitemap(),
     image({
-      serviceEntryPoint: '@astrojs/image/sharp',
+      serviceEntryPoint: "@astrojs/image/sharp",
     }),
     mdx(),
 
     ...whenExternalScripts(() =>
       partytown({
-        config: { forward: ['dataLayer.push'] },
+        config: { forward: ["dataLayer.push"] },
       })
     ),
 
@@ -65,7 +69,7 @@ export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src'),
+        "~": path.resolve(__dirname, "./src"),
       },
     },
   },
